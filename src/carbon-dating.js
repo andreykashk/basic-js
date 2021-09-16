c0730475e8f96193b477a9f100b4ea07d2290da9
AndreyKa <andreykashk@gmail.com>
2021-09-16 08:48:32 +0300

    feat: carbon-dating.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,12 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function dateSample(sampleActivity) {
+
+  if (isNaN(sampleActivity) || isNaN(parseFloat(sampleActivity)) || typeof sampleActivity != 'string' || +sampleActivity <= 0 || +sampleActivity > MODERN_ACTIVITY ) {
+    
+    return false;
+  }
+  const res = Math.ceil((Math.log(MODERN_ACTIVITY/+sampleActivity))/ (0.693/HALF_LIFE_PERIOD));
+  return res;
 }
